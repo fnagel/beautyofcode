da743c671d88a4d3a766ed9879d6141506d99cc1
Tommy Juhnke <tommy@van-tomas.de>
2013-11-05 20:08:46 +0100

    [BUGFIX] missing semicolon

--- ext_localconf.php
+++ ext_localconf.php
@@ -15,6 +15,6 @@ $dynamicTCEFormFields = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extP
 include_once($dynamicTCEFormFields);
 
 // BE preview
-$backendPreviewHook = 'EXT:beautyofcode/Classes/Hooks/class.tx_beautyofcode_pi1_cms_layout.php:tx_beautyofcode_cms_layout->getExtensionSummary'
+$backendPreviewHook = 'EXT:beautyofcode/Classes/Hooks/class.tx_beautyofcode_pi1_cms_layout.php:tx_beautyofcode_cms_layout->getExtensionSummary';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['beautyofcode_pi1'][] = $backendPreviewHook;
 ?>

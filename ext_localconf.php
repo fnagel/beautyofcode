2337e4aff5c6c67b31488604deb901168b9d5713
Tommy Juhnke <tommy@van-tomas.de>
2013-11-23 18:50:22 +0100

    [BUGFIX] fixing page layout view hooks path

--- ext_localconf.php
+++ ext_localconf.php
@@ -15,7 +15,7 @@ if (!defined ('TYPO3_MODE')) {
 );
 
 // BE preview
-$backendPreviewHook = 'EXT:beautyofcode/Classes/Hooks/PageLayoutViewHook.php:TYPO3\Beautyofcode\Hooks\PageLayoutViewHookS->getExtensionSummary';
+$backendPreviewHook = 'EXT:beautyofcode/Classes/Hooks/PageLayoutViewHooks.php:TYPO3\Beautyofcode\Hooks\PageLayoutViewHooks->getExtensionSummary';
 $TYPO3_CONF_VARS['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['beautyofcode_contentrenderer'][] = $backendPreviewHook;
 
 // cache registration

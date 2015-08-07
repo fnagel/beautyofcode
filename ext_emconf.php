b3eade3f040977d3a165b1d0fc551c7bca425976
Thomas Juhnke <tommy@van-tomas.de>
2015-08-07 15:14:40 +0200

    [FIX] Follow-up for typo3 dependency constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,11 +33,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.3.1-7.3.99',
+			'typo3' => '6.2.0-7.3.99',
 			'extbase' => '',
 			'fluid' => '',
 		),
 		'conflicts' => array(
+			'typo3' => '7.3.0',
 		),
 		'suggests' => array(
 			't3editor' => '',

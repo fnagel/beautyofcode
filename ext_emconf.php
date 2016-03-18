8b346b741b1488265ffc1e7e6a44e8002e308bde
Felix Nagel <info@felixnagel.com>
2016-03-18 21:52:20 +0100

    [TASK] Add PHP dependency constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,9 +29,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_email' => 'info@felixnagel.com',
 	'constraints' => array(
 		'depends' => array(
+			'php' => '5.4.0-7.0.99',
 			'typo3' => '7.3.1-7.6.99',
-			'extbase' => '',
-			'fluid' => '',
 		),
 		'conflicts' => array(
 		),

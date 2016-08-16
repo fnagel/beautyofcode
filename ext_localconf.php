d7dcb9f36ab42ff4e5ad318136a3c1eba7c5da6a
Felix Nagel <info@felixnagel.com>
2016-08-16 12:03:25 +0200

    [TASK] Improve extension configuration

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,36 +4,39 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'TYPO3.'.$_EXTKEY,
-    'ContentRenderer',
-    array(
-        'Content' => 'render',
-    ),
-    array(),
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
-);
-
-// BE preview
-$backendPreviewHook = 'EXT:beautyofcode/Classes/Hooks/PageLayoutViewHooks.php:TYPO3\Beautyofcode\Hooks\PageLayoutViewHooks';
-$TYPO3_CONF_VARS['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][] = $backendPreviewHook;
-
-// Cache registration
-if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_beautyofcode'])) {
-    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['cache_beautyofcode'] = array();
-}
-if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_beautyofcode']['backend'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_beautyofcode']['backend'] =
-        'TYPO3\\CMS\\Core\\Cache\\Backend\\TransientMemoryBackend';
-}
-if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_beautyofcode']['frontend'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_beautyofcode']['frontend'] =
-        'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend';
-}
-
-// Dynamic changing of t3editor format for TYPO3 7.x
-if (version_compare(TYPO3_branch, '7.4', '>=')) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\T3editor\\Form\\Element\\T3editorElement'] = array(
-        'className' => 'TYPO3\\Beautyofcode\\Form\\Element\\T3editorElement',
+call_user_func(function ($packageKey) {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'TYPO3.'.$packageKey,
+        'ContentRenderer',
+        array(
+            'Content' => 'render',
+        ),
+        array(),
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_CONTENT_ELEMENT
     );
-}
+
+    // BE preview
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem'][] =
+        'EXT:beautyofcode/Classes/Hooks/PageLayoutViewHooks.php:TYPO3\Beautyofcode\Hooks\PageLayoutViewHooks';
+
+    // Cache registration
+    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_beautyofcode'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_beautyofcode'] = array();
+    }
+    if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_beautyofcode']['backend'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_beautyofcode']['backend'] =
+            'TYPO3\\CMS\\Core\\Cache\\Backend\\TransientMemoryBackend';
+    }
+    if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_beautyofcode']['frontend'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cache_beautyofcode']['frontend'] =
+            'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend';
+    }
+
+    // Dynamic changing of t3editor format for TYPO3 7.x
+    if (version_compare(TYPO3_branch, '7.4', '>=')) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\T3editor\\Form\\Element\\T3editorElement'] = array(
+            'className' => 'TYPO3\\Beautyofcode\\Form\\Element\\T3editorElement',
+        );
+    }
+}, $_EXTKEY);
+
--- ext_tables.php
+++ ext_tables.php
@@ -4,68 +4,75 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
-    'Configuration/TypoScript/SyntaxHighlighter/',
-    'beautyOfCode (SyntaxHighlighter)'
-);
+call_user_func(function ($packageKey) {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        $packageKey,
+        'Configuration/TypoScript/SyntaxHighlighter/',
+        'beautyOfCode (SyntaxHighlighter)'
+    );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
-    'Configuration/TypoScript/Prism/',
-    'beautyOfCode (Prism)'
-);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        $packageKey,
+        'Configuration/TypoScript/Prism/',
+        'beautyOfCode (Prism)'
+    );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:beautyofcode/Configuration/TypoScript/pageTsConfig.ts">'
-);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $packageKey . '/Configuration/TypoScript/pageTsConfig.ts">'
+    );
 
-$TCA['tt_content']['columns']['CType']['config']['items'][] = array(
-    'LLL:EXT:beautyofcode/Resources/Private/Language/locallang_db.xlf:content_element.beautyofcode_contentrenderer',
-    'beautyofcode_contentrenderer',
-    'content-special-html',
-);
-$TCA['tt_content']['types']['beautyofcode_contentrenderer']['showitem'] = '
+    $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['items'][] = array(
+        'LLL:EXT:beautyofcode/Resources/Private/Language/locallang_db.xlf:content_element.beautyofcode_contentrenderer',
+        'beautyofcode_contentrenderer',
+        'content-special-html',
+    );
+
+    $GLOBALS['TCA']['tt_content']['types']['beautyofcode_contentrenderer']['showitem'] = '
 		--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.general;general,
 		--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.header;header,
-	--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.plugin,
-		pi_flexform,
-		bodytext,
-	--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
-		--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
-	--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,
-		--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.visibility;visibility,
-		--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
-	--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.extended
-';
-$TCA['tt_content']['types']['beautyofcode_contentrenderer']['columnsOverrides'] = array(
-    'bodytext' => array(
-        'label' => 'LLL:EXT:beautyofcode/Resources/Private/Language/locallang_db.xlf:code',
-        'config' => array(
-            'format' => 'mixed',
-            'renderType' => 't3editor',
-        ),
-    ),
-);
-$TCA['tt_content']['types']['list']['subtypes_addlist']['beautyofcode_contentrenderer'] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    '*',
-    'FILE:EXT:'.$_EXTKEY.'/Configuration/Flexform/ContentRenderer.xml',
-    'beautyofcode_contentrenderer'
-);
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.plugin,
+            pi_flexform,
+            bodytext,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.frames;frames,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.access,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.visibility;visibility,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.extended
+    ';
 
-// this dummy data is necessary to allow the extbase data mapper to work
-$TCA['tx_beautyofcode_domain_model_flexform'] = array(
-    'ctrl' => array(
-        'hideTable' => true,
-    ),
-    'columns' => array(
-        'c_label' => array('config' => array()),
-        'c_lang' => array('config' => array()),
-        'c_highlight' => array('config' => array()),
-        'c_collapse' => array('config' => array()),
-        'c_gutter' => array('config' => array()),
-        'uid' => array('config' => array()),
-        'pid' => array('config' => array()),
-    ),
-);
+    $GLOBALS['TCA']['tt_content']['types']['beautyofcode_contentrenderer']['columnsOverrides'] = array(
+        'bodytext' => array(
+            'label' => 'LLL:EXT:beautyofcode/Resources/Private/Language/locallang_db.xlf:code',
+            'config' => array(
+                'format' => 'mixed',
+                'renderType' => 't3editor',
+            ),
+        ),
+    );
+
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['beautyofcode_contentrenderer'] = 'pi_flexform';
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+        '*',
+        'FILE:EXT:' . $packageKey.  '/Configuration/Flexform/ContentRenderer.xml',
+        'beautyofcode_contentrenderer'
+    );
+
+    // this dummy data is necessary to allow the extbase data mapper to work
+    // @todo Check if this is still needed
+    $GLOBALS['TCA']['tx_beautyofcode_domain_model_flexform'] = array(
+        'ctrl' => array(
+            'hideTable' => true,
+        ),
+        'columns' => array(
+            'c_label' => array('config' => array()),
+            'c_lang' => array('config' => array()),
+            'c_highlight' => array('config' => array()),
+            'c_collapse' => array('config' => array()),
+            'c_gutter' => array('config' => array()),
+            'uid' => array('config' => array()),
+            'pid' => array('config' => array()),
+        ),
+    );
+
+}, $_EXTKEY);

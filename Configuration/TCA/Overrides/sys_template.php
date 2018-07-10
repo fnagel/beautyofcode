3d80c357060adf109af9e33f2d32a95b545f1b68
Manuel Munz <munz@comuno.net>
2018-07-10 19:21:04 +0200

    move TCA related stuff from ext_tables.php to TCA/Overrides/, fixes #17

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+call_user_func(function () {
+
+    $packageKey = 'beautyofcode';
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        $packageKey,
+        'Configuration/TypoScript/SyntaxHighlighter/',
+        'beautyOfCode (SyntaxHighlighter)'
+    );
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        $packageKey,
+        'Configuration/TypoScript/Prism/',
+        'beautyOfCode (Prism)'
+    );
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $packageKey . '/Configuration/TypoScript/pageTsConfig.ts">'
+    );
+});

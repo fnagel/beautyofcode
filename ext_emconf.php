25ebb95c8c69fad08bc670de5e9f7d0fa9816cec
Sebastian Fischer <typo3@evoweb.de>
2018-12-23 09:39:20 +0100

    Change compatibility to doctrine based core

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,15 +1,5 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "beautyofcode".
- *
- * Auto generated 05-09-2013 20:25
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'beautyOfCode Syntax Highlighter',
     'description' => 'This plugin provides Java-Script based, state-of-the-art, feature rich syntax highlighting by using SyntaxHighlighter or Prism. t3editor enabled.',
@@ -29,18 +19,13 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@felixnagel.com',
     'constraints' => [
         'depends' => [
-            'php' => '5.4.0-7.2.99',
-            'typo3' => '7.3.1-8.7.99',
+            'php' => '7.0.0-7.2.99',
+            'typo3' => '8.7.0-9.5.99',
         ],
         'conflicts' => [
         ],
         'suggests' => [
             't3editor' => '',
-        ],
-    ],
-    'autoload' => [
-        'psr-4' => [
-            'TYPO3\\Beautyofcode\\' => 'Classes',
         ],
     ],
 ];

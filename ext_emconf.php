dff38f11b33a9a4d0ca312c68a82657bbc28d809
Felix Nagel <info@felixnagel.com>
2019-10-02 18:54:00 +0200

    [!!!][TASK] Remove TYPO3 7.x support, Allow TYPO3 CMS 9.x, Remove PHP 5.x support

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,8 +29,8 @@ $EM_CONF[$_EXTKEY] = array(
     'author_email' => 'info@felixnagel.com',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.4.0-7.2.99',
-            'typo3' => '7.3.1-8.7.99',
+            'php' => '7.0.0-7.2.99',
+            'typo3' => '8.7.0-9.5.99',
         ),
         'conflicts' => array(
         ),

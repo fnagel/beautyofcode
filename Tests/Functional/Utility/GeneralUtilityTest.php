cfd9c43d822f0f78367fc94f8b2dea678c9167bd
Felix Nagel <info@felixnagel.com>
2019-10-29 22:55:10 +0100

    [TASK] Adopt license

--- Tests/Functional/Utility/GeneralUtilityTest.php
+++ Tests/Functional/Utility/GeneralUtilityTest.php
@@ -2,35 +2,17 @@
 
 namespace FelixNagel\Beautyofcode\Tests\Functional\Utility;
 
-/***************************************************************
- * Copyright notice
+/**
+ * This file is part of the "beautyofcode" Extension for TYPO3 CMS.
  *
- * (c) 2013 ...
- *
- * All rights reserved
- *
- * This script is part of the TYPO3 project. The TYPO3 project is
- * free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * The GNU General Public License can be found at
- * http://www.gnu.org/copyleft/gpl.html.
- *
- * This script is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
 
 /**
  * Tests the general utility class.
  *
  * @author Thomas Juhnke <typo3@van-tomas.de>
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  *
  * @link http://www.van-tomas.de/
  */

4b72cef18508f348a3e704795dd90ca73c209c3a
Felix Nagel <info@felixnagel.com>
2021-12-03 20:30:01 +0100

    [CLEANUP] Remove unneeded TYPO3_MODE constant

--- ext_tables.php
+++ ext_tables.php
@@ -1,15 +1,13 @@
 <?php
 defined('TYPO3') || die();
 
-if (TYPO3_MODE === 'BE') {
-    // Add icons to registry
-    /* @var $iconRegistry \TYPO3\CMS\Core\Imaging\IconRegistry */
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Imaging\IconRegistry::class
-    );
-    $iconRegistry->registerIcon(
-        'extensions-beautyofcode',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => 'EXT:beautyofcode/Resources/Public/Icons/Extension.svg']
-    );
-}
+// Add icons to registry
+/* @var $iconRegistry \TYPO3\CMS\Core\Imaging\IconRegistry */
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+	\TYPO3\CMS\Core\Imaging\IconRegistry::class
+);
+$iconRegistry->registerIcon(
+	'extensions-beautyofcode',
+	\TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+	['source' => 'EXT:beautyofcode/Resources/Public/Icons/Extension.svg']
+);

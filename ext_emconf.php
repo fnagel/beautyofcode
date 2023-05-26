eda6a34eb76a87f641cba12725cc5358423af641
Felix Nagel <info@felixnagel.com>
2023-05-26 16:02:46 +0200

    [!!!][TASK] Add TYPO3 12.x and remove TYPO3 10.x support

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,14 +14,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'beautyOfCode Syntax Highlighter',
     'description' => 'This plugin provides Java-Script based, state-of-the-art, feature rich syntax highlighting by using SyntaxHighlighter or Prism. t3editor enabled.',
     'category' => 'plugin',
-    'version' => '5.2.1-dev',
+    'version' => '6.0.0-dev',
     'state' => 'stable',
     'author' => 'Felix Nagel',
     'author_email' => 'info@felixnagel.com',
     'constraints' => [
         'depends' => [
-			'php' => '7.4.0-8.1.99',
-			'typo3' => '10.4.0-11.5.99',
+			'php' => '8.0.0-8.2.99',
+			'typo3' => '11.5.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [

9cbb141aa851fa488360731288607552dadbf89d
Felix Nagel <info@felixnagel.com>
2024-09-23 23:11:35 +0200

    [TASK] Allow TYPO3 version >= 13.3 only
    Disallow older TYPO3 versions due to breaking changes in flexform,
    TS generation and editor.

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,12 +20,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@felixnagel.com',
     'constraints' => [
         'depends' => [
-            'php' => '8.1.0-8.3.99',
-            'typo3' => '12.4.0-13.4.99',
+            'php' => '8.2.0-8.3.99',
+            'typo3' => '13.3.0-13.4.99',
         ],
         'conflicts' => [],
-        'suggests' => [
-            't3editor' => '',
-        ],
+        'suggests' => [],
     ],
 ];

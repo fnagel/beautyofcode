2ca882ab14f86ef261dc08325f4e35726d83f03e
Felix Nagel <info@felixnagel.com>
2026-05-20 09:16:02 +0200

    [TASK] Add enable flag for automatic CSP mutation
    Follow-up to 4afc219ce736fb0a5e0b539e7e53f62938bf85cc
    See #46

--- Classes/ContentSecurityPolicy/EventListener.php
+++ Classes/ContentSecurityPolicy/EventListener.php
@@ -4,9 +4,11 @@ namespace FelixNagel\Beautyofcode\ContentSecurityPolicy;
 
 use FelixNagel\Beautyofcode\Service\SettingsService;
 use TYPO3\CMS\Core\Attribute\AsEventListener;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Directive;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\Event\PolicyMutatedEvent;
 use TYPO3\CMS\Core\Security\ContentSecurityPolicy\SourceKeyword;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 #[AsEventListener(
     identifier: 'felixnagel-beautyofcode/mutate-csp',
@@ -20,6 +22,11 @@ class EventListener
     public function __invoke(PolicyMutatedEvent $event): void
     {
         if ($event->scope->type->isBackend()) {
+            return;
+        }
+
+        $configuration = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('beautyofcode');
+        if (!isset($configuration['enable_csp']) || $configuration['enable_csp'] == 0) {
             return;
         }
 

f64637b14f080a106dd9f8753b4d8a6a1279289d
Felix Nagel <info@felixnagel.com>
2026-08-21 18:17:27 +0200

    [TASK] Use new icon registration and remove obsolete ext_tables file
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/14.3/Deprecation-109438-ExtTablesPhpInExtensions.html
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/11.4/Feature-94692-RegisteringIconsViaServiceContainer.html#

---
+++ Configuration/Icons.php
@@ -0,0 +1,10 @@
+<?php
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'extensions-beautyofcode' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:beautyofcode/Resources/Public/Icons/Extension.svg',
+    ],
+];
